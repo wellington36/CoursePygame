3581e0b3f2361b2038efaa304d71b3fce836ed80
wellington36 <wellington.71319@gmail.com>
2022-02-07 05:11:47 -0300

    Add moviment

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ pygame.display.set_icon(icon)
 playerimg = pygame.image.load('spaceship.png')
 playerX = 370
 playerY = 480
+playerX_change = 0
 
 
 def player(x, y):
@@ -27,11 +28,22 @@ def player(x, y):
 running = True
 while running:
     screen.fill((0, 0, 0))
-    playerX += 0.1
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+        # keystroke check
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_change = - 0.3
+            if event.key == pygame.K_RIGHT:
+                playerX_change = 0.3
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                print("Keystoke has been released")
+
+    playerX += playerX_change
 
     player(playerX, playerY)
     pygame.display.update()
